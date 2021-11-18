9df127003d25c1268ccbd2e3995aa429596f11f3
3lfive <denystry@gmail.com>
2021-11-18 16:53:17 +0200

    бб

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,16 @@ import Axios from 'axios';
 function App() {
 
   const [yalantis, yalantisData] = useState("")
-
+  const alpabet =[...String.fromCharCode(...Array(123).keys()).slice(97)];
+  console.log(alpabet);
   const getYalantisData = () => {
     Axios.get('https://yalantis-react-school-api.yalantis.com/api/task0/users').then((resoponse)=>{
       console.log(resoponse);
       yalantisData(resoponse.data.map(e=><div> {e.firstName} {e.lastName};</div>))
     })
-    
+
   }
-  return (<div> list:    <button onClick={getYalantisData}> get Data </button> {yalantis}  </div>);
+  return (<div> list:    <button onClick={getYalantisData}> get Data </button> {yalantis} {alpabet.map(e=><div>{e.toUpperCase()}</div>)} </div>);
 
 }
 
