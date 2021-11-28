51841ef1517765dadd39b5a3567083e9edc6c839
3dlfive <denystry@gmail.com>
2021-11-28 22:06:24 +0200

    work versiion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,39 @@ import Axios from 'axios';
 
 function App() {
 
-  const [yalantis, yalantisData] = React.useState("")
+  const [yalantis, yalantisData] = React.useState([])
 
 
-  const alpabet =[...String.fromCharCode(...Array(123).keys()).slice(97)];
+  const alpabet =[...String.fromCharCode(...Array(123).keys()).slice(97).toUpperCase()];
 
 React.useEffect(() => {
     Axios.get('https://yalantis-react-school-api.yalantis.com/api/task0/users').then((resoponse)=>{
-      yalantisData(resoponse.data.map(e=><div> <p>{e.firstName} {e.lastName}</p></div>))
+      yalantisData(resoponse.data)
     })
 
   })
+  const filterYantis = yalantis.sort(function (a, b) {
+  if (a.lastName > b.lastName) {
+    return 1;
+  }
+  if (a.lastName < b.lastName) {
+    return -1;
+  }
+  // a должно быть равным b
+  return 0;
+});
+  console.log(yalantisData);
+  return (<div> list:    //фильтрация
+  {alpabet.map(e=> {
+    const filtD = filterYantis.filter(el=>el.lastName[0]===e)
+    console.log(filtD);
+    return (
+    <div>
 
-  return (<div> list:   {yalantis.filter(i=> i.lastName.charAt(0)=="A")} //фильтрация
-  {alpabet.map(e=><div>{e.toUpperCase()}
+    {e}
+    {filtD.map(e=> e.lastName+" "+e.firstName)}
   </div>)}
+)}
   </div>);
 
 }
